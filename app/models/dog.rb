002f46c08d2fe8973c05c7f067f47615cc605e3f
Richard Lee <richlee81192@gmail.com>
2020-01-17 22:38:04 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,16 @@
-class radio_button_tag
+class Dog 
+  attr_accessor :name, :breed, :age
+  
+  @@dogs = []
+  
+  def initialize(name:, breed:, age:)
+    @name = name
+    @breed = breed 
+    @age = age
+    @@dogs << self
+  end
+  
+  def self.all
+    @@dogs
+  end 
 end
